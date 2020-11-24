ba4a866a1704672f5db31bcde132c41a23d857ca
Silviu-Mihnea Cucuiet <40487014+SilviuMihnea@users.noreply.github.com>
2020-11-24 18:04:00 +0200

    Remove mongodb

--- postgres/scripts/003_insert_user_data.sql
+++
@@ -1,6 +0,0 @@
-insert into users.users (first_name, last_name) values 
-('Silviu-Mihnea', 'Cucuiet'),  
-('Alexandra', 'Costrâș'), 
-('Florina-Ioana', 'Cărbune'), 
-('Alexandra-Amalia', 'Bunea'),
-('Real', 'Person');
