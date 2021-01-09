632709266561a16c3c02014b273c3112034b6154
Silviu-Mihnea Cucuiet <40487014+SilviuMihnea@users.noreply.github.com>
2021-01-09 19:43:03 +0200

    New mock data

--- postgres/scripts/008_mock_applications.sql
+++ postgres/scripts/008_mock_applications.sql
@@ -998,10 +998,3 @@ insert into applications (internship_id, student_id, status) values (279, 82, 1)
 insert into applications (internship_id, student_id, status) values (243, 78, 3);
 insert into applications (internship_id, student_id, status) values (870, 43, 3);
 insert into applications (internship_id, student_id, status) values (986, 120, 1);
-
-update internships
-set description = text('Do you want to lean how to ') ||
-                  description ||
-                  text('? Come to use, and we will teach you how! Our internship is paid with: ') ||
-                  text(id * 10) ||
-                  text(' euro/day');
